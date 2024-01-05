442e2b1c038983a21af3e1503747fe2817158dce
Alexandru <alexandrulupaescu@gmail.com>
2024-01-05 21:09:38 +0200

    added some code to git

--- ui/ui.go
+++ ui/ui.go
@@ -2,13 +2,15 @@ package ui
 
 import (
 	"fmt"
+	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
-
-
 
 func mainWindow(app fyne.App) {
 	window := app.NewWindow("Go Notes")
@@ -17,10 +19,6 @@ func mainWindow(app fyne.App) {
 
 	headerContainer := container.NewVBox(header)
 	headerContainer.Layout = layout.NewCenterLayout()
-
-	apiContent := container.NewVBox(message)
-	apiContent.Resize(fyne.NewSize(400, apiContent.MinSize().Height))
-	apiContent.Layout = layout.NewCenterLayout()
 
 	// toolbar
 
@@ -38,7 +36,34 @@ func mainWindow(app fyne.App) {
 		}),
 	)
 
-	window.SetContent(container.NewVBox(toolbar, headerContainer, apiContent))
+	// notes container
+	// notesCanvas := container.NewWithoutLayout(widget.NewLabel(""))
+
+	// Sample notes
+	notes := []string{
+		"Note 1: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		"Note 2: Consectetur adipiscing elit.",
+		"Note 3: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		// Add more notes as needed
+	}
+
+	// Create a container for holding the cards
+	cardsContainer := container.NewVBox()
+
+	// Add cards to the container
+	for _, note := range notes {
+		card := createNoteCard(note)
+		cardsContainer.Add(card)
+	}
+
+	// Set the container alignment to the right
+	cardsContainer.Layout = layout.NewVBoxLayout()
+	// Create a scroll container to allow scrolling if there are too many notes
+	scrollContainer := container.NewScroll(cardsContainer)
+	scrollContainer.SetMinSize(fyne.NewSize(1280, 500))
+
+	// bottom form container
+	window.SetContent(container.NewVBox(toolbar, headerContainer, message, scrollContainer))
 
 	window.Resize(fyne.NewSize(1280, 720))
 	window.SetFixedSize(true)
@@ -46,7 +71,21 @@ func mainWindow(app fyne.App) {
 	window.Show()
 }
 
+func createNoteCard(note string) fyne.CanvasObject {
+	card := widget.NewCard("", "", widget.NewLabel(note))
+	card.Resize(fyne.NewSize(200, card.MinSize().Height))
 
+	// Create a VScroll container for vertical scrolling
+	scrollContainer := container.NewVScroll(card)
+
+	// Set the horizontal scrolling policy to ScrollBarAlwaysOff
+	scrollContainer.SetMinSize(fyne.NewSize(1280, 100))
+
+	// Create a general container for the card
+	cardContainer := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), scrollContainer)
+
+	return cardContainer
+}
 
 func App() {
 	app := app.NewWithID("go-notes")
@@ -61,6 +100,3 @@ func App() {
 	mainWindow(app)
 	app.Run()
 }
-
-
-
